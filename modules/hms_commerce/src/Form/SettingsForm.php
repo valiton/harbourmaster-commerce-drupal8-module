6f84946654baa2c15d720bb8928dd6fccb2072f5
Pawel G <contact@gbyte.co>
2016-07-13 12:22:35 +0200

    Add configuration form for altering commerce API URL

--- modules/hms_commerce/src/Form/SettingsForm.php
+++ modules/hms_commerce/src/Form/SettingsForm.php
@@ -4,6 +4,7 @@ namespace Drupal\hms_commerce\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\UrlHelper;
 
 /**
  * SettingsFrom
@@ -28,6 +29,12 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['api_source'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Commerce API URL'),
+      '#default_value' => $this->config('hms_commerce.settings')->get('api_source'),
+      '#description' => $this->t('Absolute URL to the commerce API.'),
+    );
     return parent::buildForm($form, $form_state);
   }
 
@@ -35,12 +42,18 @@ class SettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
+    if (!UrlHelper::isValid($form_state->getValue('api_source'), TRUE)) {
+      $form_state->setErrorByName('', t("<em>@path</em> is not a valid URL.", ['@path' => $form_state->getValue('api_source')]));
+    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $config = \Drupal::service('config.factory')->getEditable('hms_commerce.settings');
+    $config->set('api_source', $form_state->getValue('api_source'))
+      ->save();
     parent::submitForm($form, $form_state);
   }
 }
