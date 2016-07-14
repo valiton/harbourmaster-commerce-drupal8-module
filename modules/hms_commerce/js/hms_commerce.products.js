20a3e6a8135ebde13e1c4f3f9d46503bdb84f877
Pawel G <contact@gbyte.co>
2016-07-14 13:44:53 +0200

    Make hms_commerce.proudct.js display widgets within the new field display

--- modules/hms_commerce/js/hms_commerce.products.js
+++ modules/hms_commerce/js/hms_commerce.products.js
@@ -8,19 +8,25 @@
 
   Drupal.behaviors.hms_commerceProducts = {
     attach: function(context, settings) {
+
+      // Configure widgets.
       var apiSource = settings.hms_commerce.api_source;
       window._digtapq = window._digtapq || [];
       window._digtapq.push(['configure', {
         api: apiSource
       }]);
 
-      //window._digtapq.push(['render', {
-      //  widget: 'PremiumDownload',
-      //  selector: '#digtap-widget',
-      //  options: {
-      //    product_id: 1
-      //  }
-      //}]);
+      // Place widgets into product divs created by Drupal.
+      var productIds = settings.hms_commerce.product_ids;
+      $.each(productIds, function(index, value) {
+        window._digtapq.push(['render', {
+          widget: 'PremiumDownload',
+          selector: '#digtap-widget-' + value,
+          options: {
+            product_id: value
+          }
+        }]);
+      });
     }
   };
 })(jQuery);
