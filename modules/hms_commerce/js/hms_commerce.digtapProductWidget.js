12b1f4d5a66eccafee0766792d935f21a64bacef
Pawel G <contact@gbyte.co>
2016-07-19 11:44:25 +0200

    Add provisional js behaviours to digtap product form widget

---
+++ modules/hms_commerce/js/hms_commerce.digtapProductWidget.js
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * Attaches hms_commerce behaviors to the digtap_product field widget.
+ */
+(function($) {
+
+  "use strict";
+
+  Drupal.behaviors.hms_commerceDigtapProductWidget = {
+    attach: function(context, settings) {
+
+      // Configure widgets.
+      var apiSource = settings.hms_commerce.api_source;
+      //window._digtapq = window._digtapq || [];
+      //window._digtapq.push(['configure', {
+      //  api: apiSource
+      //}]);
+
+      $(".digtap-product-widget").click(function() {
+        //alert(this.id);
+      });
+    }
+  };
+})(jQuery);
--- modules/hms_commerce/src/Plugin/Field/FieldWidget/DigtapProductWidget.php
+++ modules/hms_commerce/src/Plugin/Field/FieldWidget/DigtapProductWidget.php
@@ -21,12 +21,22 @@ class DigtapProductWidget extends WidgetBase {
    * {@inheritdoc}
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
-    $element['value'] = $element + array(
+    $element['value'] = $element + [
         '#type' => 'textfield',
         '#title' => $this->t('Select product'),
         '#default_value' => (isset($items[$delta]->value)) ? $items[$delta]->value : '',
         '#description' => t(''),
-      );
+        '#attributes' => ['class' => ['digtap-product-widget']]
+      ];
+    // Attach behaviour to field dynamically.
+    // Attach JS and its settings to any page displaying this field.
+    $api_source = \Drupal::service('hms_commerce.settings')->getApiUrl(TRUE);
+    if (!empty($api_source)) {
+      $form['#attached']['library'][] = 'hms_commerce/digtapProductWidget';
+      $form['#attached']['drupalSettings']['hms_commerce'] = [
+        'api_source' => $api_source,
+      ];
+    }
     return $element;
   }
 }
